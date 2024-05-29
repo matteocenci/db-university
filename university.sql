80a24231aa8cfd230c8497d8823b5af52d6b602e
matteocenci <matteocenci01@gmail.com>
2024-05-29 16:14:44 +0200

    feat: fifth done

--- university.sql
+++ university.sql
@@ -15,3 +15,7 @@ SELECT * FROM `students` WHERE TIMESTAMPDIFF (YEAR, date_of_birth, CURRENT_DATE(
 
 -- RISULTATO FALLITO
 SELECT * FROM courses WHERE period = 1 AND year = 1;
+
+-- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020
+
+SELECT * FROM exams WHERE date = '2020-06-20' AND TIME(hour) > '14:00:00';
