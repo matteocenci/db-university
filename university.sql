87f5fde23c8a16be1c7160bf9ea03ad1f1a3c982
matteocenci <matteocenci01@gmail.com>
2024-05-29 16:09:32 +0200

    feat: second and third working fourth not working

--- university.sql
+++ university.sql
@@ -4,3 +4,14 @@
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
 SELECT * FROM `students` WHERE YEAR(`date_of_birth`) = 1990;
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+SELECT * FROM `courses` WHERE cfu > 10;
+
+-- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * FROM `students` WHERE TIMESTAMPDIFF (YEAR, date_of_birth, CURRENT_DATE()) > 30;
+
+-- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea
+
+-- RISULTATO FALLITO
+SELECT * FROM courses WHERE period = 1 AND year = 1;
