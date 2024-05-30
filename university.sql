161da4c99f6f21dce7406ad315653c30c1685b4d
matteocenci <matteocenci01@gmail.com>
2024-05-30 16:26:54 +0200

    feat: second query done

--- university.sql
+++ university.sql
@@ -56,5 +56,7 @@ FROM students
 GROUP BY YEAR(enrolment_date);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT office_address, COUNT(*) AS number_of_teachers
+FROM teachers
+GROUP BY office_address;
 
