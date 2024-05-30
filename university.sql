fad0e51a6b20c5557c3fa7a4f2fbb4ce8dec0eae
matteocenci <matteocenci01@gmail.com>
2024-05-30 16:33:45 +0200

    feat: third query GROUP BY done

--- university.sql
+++ university.sql
@@ -60,3 +60,7 @@ SELECT office_address, COUNT(*) AS number_of_teachers
 FROM teachers
 GROUP BY office_address;
 
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+SELECT exam_id, AVG(vote) AS average_grade
+FROM exam_student
+GROUP BY exam_id;
